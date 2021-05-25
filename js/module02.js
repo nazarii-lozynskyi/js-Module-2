7d86f1da9d98f7f9f6931890f4d83f94927e6bcc
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-05-25 11:01:12 +0200

    Update module02.js

--- js/module02.js
+++ js/module02.js
@@ -443,7 +443,7 @@
 //***************** 32 */
 function includes(array, value) {
   // Change code below this line
-  for (let i = 0; i <= array.length; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
     if (array[i] === value) {
       console.log(true);
       return true;
