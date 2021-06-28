e2d053ed5e4b9804f2e5df1f9def2f63e1f26bb2
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-06-28 18:04:08 +0200

    Update module06.js

--- js/module06.js
+++ js/module06.js
@@ -1794,3 +1794,28 @@
 //   'female',
 // );
 /********************* */
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+
+  for (const product of products) {
+    if (product.name === productName) {
+      console.log(product.price * product.quantity);
+      return product.price * product.quantity;
+    }
+  }
+  // Пиши код выше этой строки
+}
+
+calculateTotalPrice('Blaster');
+calculateTotalPrice('Radar');
+calculateTotalPrice('Droid');
+calculateTotalPrice('Grip');
+calculateTotalPrice('Scanner');
